c9daf723e2f2bb4ef71ceaf54c31c9326199f2b7
Fuat Berke Gültekin <fuatberkegultekin@gmail.com>
2023-08-24 14:54:58 +0300

    API | ApiContext

---
+++ react-assignment-3/src/Context/ApiContext.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+const ApiContext = createContext();
+
+export const ApiProvider = ({ children }) => {
+  const [data, setData] = useState({});
+  const [city, setCity] = useState("");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=7d78bed4eb1cd7d9160ff512f739999f`;
+
+  useEffect(() => {
+    if (city != "") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  }, [city]);
+
+  const values = {
+    data,
+    setData,
+    setCity,
+  };
+
+  return <ApiContext.Provider value={values}>{children}</ApiContext.Provider>;
+};
+
+export default ApiContext;
