7d610d85584f24394e03737e91b56ade9c811e91
Fuat Berke Gültekin <fuatberkegultekin@gmail.com>
2023-08-27 11:17:56 +0300

    Test-1 Created

--- react-assignment-4/src/Emoji.test.js
+++ react-assignment-4/src/Emoji.test.js
@@ -4,3 +4,10 @@ import App from "./App";
 import Header from "./Header";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
+
+// TEST 1
+test("Has the Header been rendered?", () => {
+  render(<Header />);
+  const header = screen.getByText("Emoji Search");
+  expect(header).toBeInTheDocument();
+});
